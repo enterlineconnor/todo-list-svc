112ccbd33044dd4a1e3b7a16a96e55c59318e9f9
enterlineconnor <enterlineconnor@gmail.com>
2023-03-24 00:41:34 -0400

    migrate api funcions to routes folder

--- api/main.go
+++ api/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
     "log"
-
+	"api/routes"
     "github.com/gofiber/fiber/v2"
 )
 
 func main() {
     app := fiber.New()
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	routes.TodoNotes(app)
 
     log.Fatal(app.Listen(":3000"))
 }
